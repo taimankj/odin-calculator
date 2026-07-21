efb60d0787ebb5fad8cab606f0e535f614217da0
Keoni Taimanglo <taimanglo.oni@hotmail.com>
2026-07-21 12:15:37 -0700

    Implement processInput logic and wire up buttons
    - Add isSet helper to check whether all given variables are truthy
    - Add clearState to reset the output/history areas and n1, n2, op
    - Implement processInput branches for =, clear, operators, and digits
      - operators default n1 to "0" when unset, then store the operator
      - digits append to or initialize n1/n2 based on current state
      - "=" logs the evaluated expression, then clears state
    - Update pseudocode comments to match the new control flow
    - Attach click listeners that call processInput and log current state

--- script.js
+++ script.js
@@ -39,15 +39,34 @@ function operate(n1, n2, op) {
   }
 }
 
+function isSet(...variables) {
+  let isVarSet = true;
+  for (const i in variables) {
+    if (!variables[i]) isVarSet = false;
+  }
+
+  return isVarSet;
+}
+
+function clearState() {
+  historyArea.innerText = "";
+  outputArea.innerText = "";
+  n1 = "";
+  n2 = "";
+  op = "";
+}
+
 function processInput(input) {
   // if operator is equals and if n1, op, and n2 is set,
   //    process expression: n1 op n2
-  // else if input is +, *, -, or / and if n1 is initialized
-  //    set op to input
-  //    else, set n1 to 0 and op to input
+  //    set innerText for history and output area to ''
+  //    set n1, op, n2 to ''
   // else if input is 'clear'
   //    set innerText for history and output area to ''
   //    set n1, op, n2 to ''
+  // else if input is not a number (+, *, -, or /), then
+  //    if n1 is not initialized, set n1 to '0'
+  //    set op to input
   // else input is number, then
   //    if operator is set
   //        if n2 is set, then append input to n2
@@ -55,7 +74,40 @@ function processInput(input) {
   //    else,
   //        if n1 is set, then append input to n1
   //        else, set n1 to input
+
+  if (input == "=" && isSet(n1, n2, op)) {
+    console.log(
+      `Enter button clicked:\n\t${n1} ${op} ${n2} = ${operate(+n1, +n2, op)}`,
+    );
+    clearState();
+  } else if (input == "clear") {
+    clearState();
+  } else if (Number.isNaN(+input)) {
+    if (!isSet(n1)) {
+      n1 = "0";
+    }
+    op = input;
+  } else {
+    if (isSet(op)) {
+      if (isSet(n2)) {
+        n2 += input;
+      } else {
+        n2 = input;
+      }
+    } else {
+      if (isSet(n1)) {
+        n1 += input;
+      } else {
+        n1 = input;
+      }
+    }
+  }
 }
 
 // event listeners
-buttons.forEach((button) => button.addEventListener("click", (e) => {}));
+buttons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    processInput(e.target.value);
+    console.log(`Current State:\n\tn1: ${n1} n2: ${n2} op: ${op}`);
+  }),
+);
