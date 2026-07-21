bf5c0bc6f0565ff4500fc0378ff43bd512ebf76e
Keoni Taimanglo <taimanglo.oni@hotmail.com>
2026-07-21 11:42:24 -0700

    Scaffold input handling and calculator state
    removed text from calculator-output's div tags
       also changed their font size to 72px
    
    initialized n1, n2, and op to empty strings
    initialized DOM nodes for output and history div tags
    
    created new function, processInput to handle button click events
       only pseudocode written
       no logic implemented yet

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Variables for calculations
+let n1 = ""; // number 1 input
+let n2 = ""; // number 2 input
+let op = ""; // operator
+
+// DOM Nodes
+let buttons = document.querySelectorAll("button");
+let outputArea = document.querySelector("#output");
+let historyArea = document.querySelector("#history");
+
 function add(n1, n2) {
   return n1 + n2;
 }
@@ -29,12 +39,23 @@ function operate(n1, n2, op) {
   }
 }
 
-// Variables for calculations
-let n1; // number 1 input
-let n2; // number 2 input
-let op; // operator
-
-let buttons = document.querySelectorAll("button");
+function processInput(input) {
+  // if operator is equals and if n1, op, and n2 is set,
+  //    process expression: n1 op n2
+  // else if input is +, *, -, or / and if n1 is initialized
+  //    set op to input
+  //    else, set n1 to 0 and op to input
+  // else if input is 'clear'
+  //    set innerText for history and output area to ''
+  //    set n1, op, n2 to ''
+  // else input is number, then
+  //    if operator is set
+  //        if n2 is set, then append input to n2
+  //        else, set n2 to input
+  //    else,
+  //        if n1 is set, then append input to n1
+  //        else, set n1 to input
+}
 
 // event listeners
 buttons.forEach((button) => button.addEventListener("click", (e) => {}));
